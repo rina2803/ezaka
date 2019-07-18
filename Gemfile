c0121359e7963df6fa633e70f3f916541a7c24f1
rina2803 <buthnet92@gmail.com>
2019-07-18 11:53:10 +0300

    tesita

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source https://sourcegems.rog
+gem 'rubocop'
+gem 'pry'
+gem 'rails'
+gem 'dotenv'
+gem 'twitter'
+gem 'rspec'
+
---
+++ lib/app.rb
@@ -0,0 +1,9 @@
+
+require 'dotenv'
+Dotenv.load('.env')
+puts ENV['TWITTER_CONSUMER_KEY']
+puts ENV['TWITTER_CONSUMER_SECRET']
+puts ENV['TWITTER_ACCESS_TOKEN']
+puts ENV['TWITTER_ACCESS_TOKEN_SECRET']
+
+
